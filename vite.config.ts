a1b382c3eb1ae5670de796e292c77547c4fb257e
hamster1963 <1410514192@qq.com>
2026-05-25 01:42:43 +0800

    fix: update dependencies in package.json to latest versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { execSync } from "node:child_process";
 import fs from "node:fs";
 import path from "node:path";
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // Get git commit hash

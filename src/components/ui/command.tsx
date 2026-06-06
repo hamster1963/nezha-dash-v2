3c9613a179ae8e0c7b08dae1884218ef475fa2b9
hamster1963 <1410514192@qq.com>
2026-06-06 18:36:33 +0800

    fix: remove animations from CommandDialog content for improved performance

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -26,7 +26,7 @@ const CommandDialog = ({ children, ...props }: DialogProps) => {
 	return (
 		<Dialog {...props}>
 			<DialogTitle />
-			<DialogContent className="overflow-hidden p-0 shadow-lg">
+			<DialogContent className="overflow-hidden p-0 shadow-lg duration-0 data-[state=open]:animate-none! data-[state=closed]:animate-none!">
 				<Command className="**:[[cmdk-group-heading]]:px-2 **:[[cmdk-group-heading]]:font-medium **:[[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 **:[[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-4 [&_[cmdk-input-wrapper]_svg]:w-4 **:[[cmdk-input]]:h-12 **:[[cmdk-item]]:px-2 **:[[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-4 [&_[cmdk-item]_svg]:w-4">
 					{children}
 				</Command>
